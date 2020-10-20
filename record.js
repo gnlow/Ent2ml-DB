65d3b2cf4b0223efea8fb79b55722982ae4236aa
Gnlow <entrydark@gmail.com>
2020-10-20 17:39:38 +0900

    add record.js

---
+++ record.js
@@ -0,0 +1,18 @@
+let dates = []
+let users = {}
+for await (const dir of Deno.readDir("./prev")) {
+    dates.push(dir.name.substring(0, dir.name.length - 5))
+    
+    const data = JSON.parse(await Deno.readTextFile(`./prev/${dir.name}`))
+
+    data.forEach(({username, likeCount}) => {
+        if (!users[username]) {
+            users[username] = {
+                start: dates.length - 1,
+                records: []
+            }
+        }
+        users[username].records.push(likeCount - users[username].records.reduce((prev, curr) => prev + curr, 0))
+    })
+}
+Deno.writeTextFile("record.json", JSON.stringify(users))
